5ca5988a77fb858b6f8c475b11f8fa4c4d8691cc
Al-Amin Islam <jimalamin7@gmail.com>
2026-02-08 23:35:26 +0600

    setUp image field

---
+++ mentor_hub_client/src/components/modules/auth/login-form.tsx
@@ -0,0 +1,63 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"form">) {
+  return (
+    <form className={cn("flex flex-col gap-6", className)} {...props}>
+      <FieldGroup>
+        <div className="flex flex-col items-center gap-1 text-center">
+          <h1 className="text-2xl font-bold">
+            <span className="text-indigo-500">Sign Up</span> to your account
+          </h1>
+          <p className="text-muted-foreground text-sm text-balance">
+            Enter your email below to signup to your account
+          </p>
+        </div>
+        <Field>
+          <FieldLabel htmlFor="email">Email</FieldLabel>
+          <Input id="email" type="email" placeholder="m@example.com" required />
+        </Field>
+        <Field>
+          <div className="flex items-center">
+            <FieldLabel htmlFor="password">Password</FieldLabel>
+            <Link
+              href="#"
+              className="ml-auto text-sm underline-offset-4 hover:underline"
+            >
+              Forgot your password?
+            </Link>
+          </div>
+          <Input id="password" type="password" required />
+        </Field>
+        <Field>
+          <Button className="bg-indigo-500 hover:bg-indigo-600" type="submit">
+            Login
+          </Button>
+        </Field>
+
+        <Field>
+          <FieldDescription className="text-center">
+            Don&apos;t have an account?{" "}
+            <Link
+              href={"/signup"}
+              className="text-indigo-600 hover:text-indigo-700 hover:underline font-medium"
+            >
+              Sign up
+            </Link>
+          </FieldDescription>
+        </Field>
+      </FieldGroup>
+    </form>
+  );
+}
