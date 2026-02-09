ba2fda9192ee488fb39457c9653e71d8d4b8d477
Al-Amin Islam <jimalamin7@gmail.com>
2026-02-09 13:18:07 +0600

    implement sign in

--- mentor_hub_client/src/components/modules/auth/login-form.tsx
+++ mentor_hub_client/src/components/modules/auth/login-form.tsx
@@ -1,62 +1,127 @@
-import { cn } from "@/lib/utils";
+"use client";
+
 import { Button } from "@/components/ui/button";
 import {
   Field,
   FieldDescription,
+  FieldError,
   FieldGroup,
   FieldLabel,
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
+import { z } from "zod";
+import { useForm } from "@tanstack/react-form";
+import toast from "react-hot-toast";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+
+const formSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(8, "Password must be at least 8 characters"),
+});
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"form">) {
-  return (
-    <form className={cn("flex flex-col gap-6", className)} {...props}>
-      <FieldGroup>
-        <div className="flex flex-col items-center gap-1 text-center">
-          <h1 className="text-2xl font-bold">
-            <span className="text-indigo-500">Sign In</span> to your account
-          </h1>
-          <p className="text-muted-foreground text-sm text-balance">
-            Enter your email below to signin to your account
-          </p>
-        </div>
-        <Field>
-          <FieldLabel htmlFor="email">Email</FieldLabel>
-          <Input id="email" type="email" placeholder="m@example.com" required />
-        </Field>
-        <Field>
-          <div className="flex items-center">
-            <FieldLabel htmlFor="password">Password</FieldLabel>
-            <Link
-              href="#"
-              className="ml-auto text-sm underline-offset-4 hover:underline"
-            >
-              Forgot your password?
-            </Link>
-          </div>
-          <Input id="password" type="password" required />
-        </Field>
-        <Field>
-          <Button className="bg-indigo-500 hover:bg-indigo-600" type="submit">
-            Login
-          </Button>
-        </Field>
+  const router = useRouter();
 
-        <Field>
-          <FieldDescription className="text-center">
-            Don&apos;t have an account?{" "}
-            <Link
-              href={"/signup"}
-              className="text-indigo-600 hover:text-indigo-700 hover:underline font-medium"
-            >
-              Sign up
-            </Link>
-          </FieldDescription>
-        </Field>
+  const form = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    validators: {
+      onSubmit: formSchema,
+    },
+    onSubmit: async ({ value }) => {
+      const toastId = toast.loading("Creating user...");
+      try {
+        const { error } = await authClient.signIn.email(value);
+
+        if (error) {
+          toast.error(error.message || "An error occurred", {
+            id: toastId,
+          });
+          return;
+        }
+
+        toast.success("User created successfully", {
+          id: toastId,
+        });
+        router.push("/");
+      } catch (err: any) {
+        toast.error(err.message || "Something went wrong", {
+          id: toastId,
+        });
+      }
+    },
+  });
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        form.handleSubmit();
+      }}
+    >
+      <div className="flex flex-col items-center gap-1 text-center pb-10">
+        <h1 className="text-2xl font-bold">
+          <span className="text-indigo-500">Sign In</span> to your account
+        </h1>
+        <p className="text-muted-foreground text-sm text-balance">
+          Enter your email below to signin to your account
+        </p>
+      </div>
+
+      <FieldGroup>
+        <form.Field
+          name="email"
+          children={(field) => {
+            const isInvalid =
+              field.state.meta.isTouched && !field.state.meta.isValid;
+            return (
+              <Field data-invalid={isInvalid}>
+                <FieldLabel htmlFor={field.name}>Email</FieldLabel>
+                <Input
+                  id={field.name}
+                  name={field.name}
+                  value={field.state.value}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                />
+                {isInvalid && <FieldError errors={field.state.meta.errors} />}
+              </Field>
+            );
+          }}
+        ></form.Field>
+        <form.Field
+          name="password"
+          children={(field) => {
+            const isInvalid =
+              field.state.meta.isTouched && !field.state.meta.isValid;
+            return (
+              <Field data-invalid={isInvalid}>
+                <FieldLabel htmlFor={field.name}>Password</FieldLabel>
+                <Input
+                  id={field.name}
+                  name={field.name}
+                  value={field.state.value}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                  type="password"
+                />
+                {isInvalid && <FieldError errors={field.state.meta.errors} />}
+              </Field>
+            );
+          }}
+        ></form.Field>
+        <Button className="bg-indigo-400 hover:bg-indigo-600">Sign In</Button>
+        <FieldDescription className="px-6 text-center">
+          Already have an account?{" "}
+          <Link href="/signup">
+            <span className="text-indigo-600">Sign up</span>
+          </Link>
+        </FieldDescription>
       </FieldGroup>
     </form>
   );
