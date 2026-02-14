735fc3689dbe2a93d3a96366d0c8bf75762cc4e8
Al-Amin Islam <jimalamin7@gmail.com>
2026-02-14 20:06:01 +0600

    vercel file

--- MentorHub server/src/app.ts
+++ MentorHub server/src/app.ts
@@ -1,40 +1,46 @@
 import express from "express";
 import cors from "cors";
 import { toNodeHandler } from "better-auth/node";
-import { auth } from "./lib/auth.js";
-import { authRouter } from "./modules/auth/auth.route.js";
-import { tutorRouter } from "./modules/tutor/tutor.route.js";
-import { reviewRouter } from "./modules/review/review.route.js";
-import { studentRouter } from "./modules/students/student.route.js";
-import { categoryRouter } from "./modules/categories/categories.route.js";
-import { bookingsRouter } from "./modules/bookings/bookings.route.js";
-import { notFound } from "./middleware/notFound.js";
-import errorHandler from "./middleware/errorHandler.js";
+import { auth } from "./lib/auth";
+import { authRouter } from "./modules/auth/auth.route";
+import { tutorRouter } from "./modules/tutor/tutor.route";
+import { reviewRouter } from "./modules/review/review.route";
+import { studentRouter } from "./modules/students/student.route";
+import { categoryRouter } from "./modules/categories/categories.route";
+import { bookingsRouter } from "./modules/bookings/bookings.route";
+import { notFound } from "./middleware/notFound";
+import errorHandler from "./middleware/errorHandler";
 
 const app = express();
 
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.APP_URL || "*",
+    origin: process.env.APP_URL,
     credentials: true,
   }),
 );
 
 app.use("/api/auth", authRouter);
+
 app.use("/api/tutor", tutorRouter);
+
 app.use("/api/review", reviewRouter);
+
 app.use("/api/student", studentRouter);
+
 app.use("/api/category", categoryRouter);
+
 app.use("/api/booking", bookingsRouter);
 
-app.all("/api/auth/*", toNodeHandler(auth));
+app.all(/^\/api\/auth\/.*/, toNodeHandler(auth));
 
 app.get("/", (req, res) => {
   res.send({ message: "SkillBridge API Running" });
 });
 
 app.use(notFound);
+
 app.use(errorHandler);
 
 export default app;
