15335be675f91bcd541cc7345e191328aa1e3c60
Al-Amin Islam <jimalamin7@gmail.com>
2026-05-06 15:21:58 +0600

    fix next config file

--- mentor_hub_client/next.config.ts
+++ mentor_hub_client/next.config.ts
@@ -30,23 +30,7 @@ const config: any = {
       },
     ];
   },
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "X-Frame-Options",
-            value: "SAMEORIGIN",
-          },
-          {
-            key: "Content-Security-Policy",
-            value: "frame-ancestors 'self'",
-          },
-        ],
-      },
-    ];
-  },
+
 };
 
 export default config;
